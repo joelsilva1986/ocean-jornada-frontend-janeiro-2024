33ca9904ef983fa589be7fffff63e66764ba1313
Joel Silva <joel.a.silva1986@gmail.com>
2024-01-25 15:00:37 +0000

    Extração do Componente Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card (){
+
+    return (
+        <>
+            <div className="card">
+                <h2>Rick Sanchez</h2>
+                <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+            </div>
+        </>
+    )
+}
