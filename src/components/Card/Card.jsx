0279a20ad8904774ebc15584f37772998ea534d5
Joel Silva <joel.a.silva1986@gmail.com>
2024-01-30 12:49:48 +0000

    Criação e estilização das tags

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,6 +4,12 @@ export default function Card (){
         <>
             <div className="card">
                 <h2>Rick Sanchez</h2>
+                <div className="tags">
+                    <div className="tag">Status: Vivo</div>
+                    <div className="tag">Espaço: Humana</div>
+                    <div className="tag">Origem: Terra C-137</div>
+                </div>
+                
                 <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
             </div>
         </>
